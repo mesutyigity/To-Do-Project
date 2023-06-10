02c3a77967fd6ae34df08b83a053033fd5f271f1
Mesut <yildirimmesutyigit@gmail.com>
2023-06-10 15:50:00 +0300

    new commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+// *selector
+
 const input = document.getElementById("todo-input");
 const btn = document.querySelector("#todo-btn");
+const ul = document.querySelector("#todo-ul");
+
+//*
+const li = document.createElement("li");
+ul.prepend(li);
+btn.addEventListener("click", () => {
+  e.preventDefault();
+  li.textContent = input.value;
+});
