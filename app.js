484a58734d4c4b9eafa31abbd1cb0385d4fe7296
Mesut <yildirimmesutyigit@gmail.com>
2023-06-10 20:39:02 +0300

    new commit

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const btn = document.querySelector("#todo-btn");
 const ul = document.querySelector("#todo-ul");
 
 //*
-const li = document.createElement("li");
-ul.prepend(li);
+
 btn.addEventListener("click", () => {
   e.preventDefault();
+  const li = document.createElement("li");
+  ul.prepend(li);
+  const i = document.createElement("i");
+  li.appendChild(i);
+  console.log(i);
+  i.className = "fa-solid fa-trash-can -text-danger";
+  i.type = "button";
+  console.log(li);
+
   li.textContent = input.value;
+  input.value = "";
 });
