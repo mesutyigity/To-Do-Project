e875b0239ce5405df8c053770eb69f3a1a79c7aa
Mesut <yildirimmesutyigit@gmail.com>
2023-06-21 09:08:39 +0300

    new commit

--- app.js
+++ app.js
@@ -29,6 +29,22 @@ const todosContainer = document.querySelector(".todos");
 
 //*
 
+const toggleComplete = (event) => {
+  const { parentElement } = event.currentTarget;
+  const isCompleted = [...parentElement.classList].some(
+    (className) => className === "completed"
+  );
+  if (!isCompleted) event.currentTarget.innerText = "Todo";
+  else {
+    event.currentTarget.innerText = "Complete";
+  }
+  event.currentTarget.parentElement.classList.toggle("completed");
+};
+
+const removeItem = (event) => {
+  todosContainer.removeChild(event.currentTarget.parentElement);
+};
+
 const renderTodoItem = (todoText) => {
   const todoItemElement = document.createElement("li");
   todoItemElement.classList.add("todoItem");
@@ -36,15 +52,18 @@ const renderTodoItem = (todoText) => {
   const completeButton = document.createElement("button");
   completeButton.classList.add("completeButton");
   completeButton.innerText = "Complete";
+  completeButton.addEventListener("click", toggleComplete);
   todoItemElement.appendChild(completeButton);
 
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("deleteButton");
   deleteButton.innerText = "Delete";
+  deleteButton, addEventListener("click", removeItem);
   todoItemElement.appendChild(deleteButton);
 
   const textElement = document.createElement("p");
   textElement.innerText = todoText;
+  textElement.classList.add("todoText");
   todoItemElement.appendChild(textElement);
 
   todosContainer.appendChild(todoItemElement);
